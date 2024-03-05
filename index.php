9d285c79df82166cb51aa5057fabc9141b73b3f9
ARUNTEJAMENDA <aruntejamenda@gmail.com>
2024-03-05 19:09:04 +0530

    folderized cse

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main Page</title>
+</head>
+<body>
+    <ol>
+        <li><a href="./CE/" target="_blank" rel="noopener noreferrer">CE</a></li>
+        <li><a href="./ECE/" target="_blank" rel="noopener noreferrer">CSE</a></li>
+        <li><a href="./CHE/" target="_blank" rel="noopener noreferrer">CHE</a></li>
+        <li><a href="./ECE/" target="_blank" rel="noopener noreferrer">ECE</a></li>
+        <li><a href="./EE/" target="_blank" rel="noopener noreferrer">EE</a></li>
+        <li><a href="./ME/" target="_blank" rel="noopener noreferrer">ME</a></li>
+        <li><a href="./MME/" target="_blank" rel="noopener noreferrer">MME</a></li>
+    </ol>
+</body>
+</html>
