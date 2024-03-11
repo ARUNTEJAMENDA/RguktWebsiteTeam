294adb8fdd70b0a917e960333b3b93cf5723fbb3
ARUNTEJAMENDA <aruntejamenda@gmail.com>
2024-03-11 16:30:28 +0530

    yes

--- CSE/index.php
+++ CSE/index.php
@@ -12,28 +12,7 @@
 		<script src="assets/js/jquery-3.7.1.slim.min.js"></script>
 	</head>
 	<body>
-		<header>
-			<img id="logo" src="assets/img/cse_logo.jpg" alt="logo" />
-			<div id="head">
-				<p id="headTitle">Department of Computer Science & Engineering</p>
-				<p id="headDesc">Rajiv Gandhi University of Knowledge Technologies - Nuzvid, Eluru District, A.P, India.</p>
-			</div>
-			<div id="others">
-				<p><a href="#">A - Z Index</a> | <a href="#">Sitemap</a></p>
-			</div>
-		</header>
-		<nav>
-			<ol>
-				<a href="#"><li>Home</li></a>
-				<a href="#"><li>People</li></a>
-				<a href="#"><li>Research</li></a>
-				<a href="#"><li>Academics</li></a>
-				<a href="#"><li>Events</li></a>
-				<a href="#"><li>Resources</li></a>
-				<a href="#"><li>About Us</li></a>
-				<a href="#"><li>Contact Us</li></a>
-			</ol>
-		</nav>
+		<?php include "includes/headerNav.php"?>
 		<section id="main">
 			<div id="row1">
 				<div class="section1" id="updatesContainer">
